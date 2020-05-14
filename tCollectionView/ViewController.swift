fd975a1473eeee64d8fc09b7f9f478fd14d419d9
tyobigoro_i <tyobigorox@gmail.com>
2020-05-14 16:58:08 +0900

    Initial Commit

---
+++ tCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tCollectionView
+//
+//  Created by tyobigoro on 2020/05/14.
+//  Copyright © 2020 tyobigoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
